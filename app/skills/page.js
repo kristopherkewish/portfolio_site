8ca6f36f6c22571768b487a30c0b57eb35f1a231
kristopherkewish <148918881+kristopherkewish@users.noreply.github.com>
2024-04-23 14:56:42 +1000

    Get orbit animation working

--- app/skills/page.js
+++ app/skills/page.js
@@ -1,13 +1,53 @@
+"use client";
+
+import { useEffect } from "react";
+import styles from './styles.module.css';
+
+const OrbitContainer = ({ numIcons }) => {
+  useEffect(() => {
+    // Function to create and position icons around the circumference of the circular div
+    const createIcons = () => {
+      const orbitContainer = document.getElementById("orbit-container");
+      const radius = orbitContainer.offsetWidth / 2; // Radius of the circular div
+      const centerX = radius;
+      const centerY = radius;
+      const angleIncrement = (2 * Math.PI) / numIcons;
+
+      for (let i = 0; i < numIcons; i++) {
+        const angle = i * angleIncrement;
+        const icon = document.createElement("div");
+        const iconRadius = 40;
+        icon.className = styles.orbitIcon;
+        icon.style.top = (centerY - iconRadius) + radius * Math.sin(angle) + "px";
+        icon.style.left = (centerX - iconRadius) + radius * Math.cos(angle) + "px";
+        orbitContainer.appendChild(icon);
+      }
+    };
+
+    // Create icons when component mounts
+    createIcons();
+
+    
+
+    // Clean up function to remove icons when component unmounts
+    return () => {
+      const orbitContainer = document.getElementById("orbit-container");
+      orbitContainer.innerHTML = ""; // Remove all child elements
+    };
+  }, [numIcons]);
+
+  return (
+    <div
+      className={`${styles.orbitContainer} relative aspect-square h-4/6 rounded-full bg-gray-200 border-solid border-2 border-indigo-600`}
+      id="orbit-container"
+    ></div>
+  );
+};
+
 export default function Skills() {
   return (
-    <div class="relative flex h-screen	w-screen flex-col items-center justify-center bg-gray-200 text-black">
-      <div class="text-6xl font-bold">SKILLS</div>
-      <div class="absolute left-60 top-60 flex h-20 w-20 items-center justify-center rounded-full bg-blue-500"></div>
-      <div class="absolute bottom-44 left-44 flex h-20 w-20 items-center justify-center rounded-full bg-blue-500"></div>
-      <div class="absolute right-60 top-44 flex h-20 w-20 items-center justify-center rounded-full bg-blue-500"></div>
-      <div class="absolute bottom-60 right-52 flex h-20 w-20 items-center justify-center rounded-full bg-blue-500"></div>
-      <div class="absolute bottom-32 right-1/2 flex h-20 w-20 items-center justify-center rounded-full bg-blue-500"></div>
-      <div class="absolute left-1/2 top-32 flex h-20 w-20 items-center justify-center rounded-full bg-blue-500"></div>
+    <div className="flex h-screen items-center justify-center">
+      <OrbitContainer numIcons={6} />
     </div>
   );
 }
