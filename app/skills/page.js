a256bd41d3b53be573e083d440cd2ae29b714ba7
kristopherkewish <148918881+kristopherkewish@users.noreply.github.com>
2024-04-23 13:53:33 +1000

    Add skills page

---
+++ app/skills/page.js
@@ -0,0 +1,13 @@
+export default function Skills() {
+  return (
+    <div class="relative flex h-screen	w-screen flex-col items-center justify-center bg-gray-200 text-black">
+      <div class="text-6xl font-bold">SKILLS</div>
+      <div class="absolute left-60 top-60 flex h-20 w-20 items-center justify-center rounded-full bg-blue-500"></div>
+      <div class="absolute bottom-44 left-44 flex h-20 w-20 items-center justify-center rounded-full bg-blue-500"></div>
+      <div class="absolute right-60 top-44 flex h-20 w-20 items-center justify-center rounded-full bg-blue-500"></div>
+      <div class="absolute bottom-60 right-52 flex h-20 w-20 items-center justify-center rounded-full bg-blue-500"></div>
+      <div class="absolute bottom-32 right-1/2 flex h-20 w-20 items-center justify-center rounded-full bg-blue-500"></div>
+      <div class="absolute left-1/2 top-32 flex h-20 w-20 items-center justify-center rounded-full bg-blue-500"></div>
+    </div>
+  );
+}
