820e721444f64e400f7d3cb290897622c0e54690
kristopherkewish <148918881+kristopherkewish@users.noreply.github.com>
2024-05-05 16:59:01 +1000

    Remove moving dot animation from Projects

--- app/projects/Button.js
+++
@@ -1,7 +0,0 @@
-export default function Button() {
-    return (
-        <div className="absolute bottom-3 mt-5 w-fit cursor-pointer self-center rounded-full bg-slate-900 px-5 py-3 text-white transition hover:bg-slate-400 hover:text-slate-900 active:bg-slate-900 active:text-slate-200">
-          Open App
-        </div>
-    )
-}
