7b52ea272152cc1f3257b48bfa54ec32033b0003
kristopherkewish <148918881+kristopherkewish@users.noreply.github.com>
2024-05-05 20:37:46 +1000

    Split Timeline into separate components

---
+++ app/experience/TimelineHoverButtons.js
@@ -0,0 +1,32 @@
+import { roles } from "./roles.js";
+
+export default function TimelineHoverButtons({ handleHover }) {
+  return (
+    <div className="flex h-24 w-5/6 bg-white drop-shadow-xl">
+      <div
+        onMouseEnter={() => handleHover(roles.monash)}
+        className="flex h-full w-1/2 items-center bg-emerald-400 p-5 text-lg font-bold text-white hover:cursor-pointer hover:bg-emerald-100 hover:text-black"
+      >
+        2018
+      </div>
+      <div
+        onMouseEnter={() => handleHover(roles.hardchrome)}
+        className="flex h-full w-1/6 items-center bg-rose-400 p-5 text-lg font-bold text-white hover:cursor-pointer hover:bg-rose-100 hover:text-black"
+      >
+        2021
+      </div>
+      <div
+        onMouseEnter={() => handleHover(roles.calix)}
+        className="flex h-full w-1/6 items-center bg-yellow-400 p-5 text-lg font-bold text-white hover:cursor-pointer hover:bg-yellow-100 hover:text-black"
+      >
+        2022
+      </div>
+      <div
+        onMouseEnter={() => handleHover(roles.present)}
+        className="flex h-full w-1/6 items-center bg-blue-400 p-5 text-lg font-bold text-white hover:cursor-pointer hover:bg-blue-100 hover:text-black"
+      >
+        2024
+      </div>
+    </div>
+  );
+}
---
+++ app/experience/TimelineInfo.js
@@ -0,0 +1,19 @@
+export default function TimelineInfo({ role }) {
+  return (
+    <div className="mb-10 flex h-1/3 w-1/2 flex-col justify-center overflow-hidden rounded-lg drop-shadow-lg">
+      <div className="rounded-t-lg bg-emerald-900 p-3 text-white">
+        <div className="font-xl font-bold">{role.title}</div>
+        <div className="font-l italic">{role.company}</div>
+      </div>
+      <div className="h-full rounded-b-lg bg-white p-3 text-black">
+        <div className="font-normal">
+          <ul>
+            {role.descriptionPoints.map((descriptionPoint, index) => (
+              <li key={index}>{descriptionPoint}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
