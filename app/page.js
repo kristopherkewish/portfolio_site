f1e84d589bf22f59eb85661fb877f830eb98ac72
kristopherkewish <148918881+kristopherkewish@users.noreply.github.com>
2024-05-05 20:45:12 +1000

    Link Projects and Skills pages

--- app/page.js
+++ app/page.js
@@ -8,8 +8,8 @@ export default function Home() {
       <div class="text-[10rem] font-extrabold">KRIS KEWISH</div>
       <TypedText />
       <Link href="/skills"><div class="absolute left-8 top-8 text-xl">Skills</div></Link>
-      <div class="absolute right-8 top-8 text-xl">Experience</div>
-      <div class="absolute bottom-8  left-8 text-xl">Projects</div>
+      <Link href="/experience"><div class="absolute right-8 top-8 text-xl">Experience</div></Link>
+      <Link href="/projects"><div class="absolute bottom-8  left-8 text-xl">Projects</div></Link>
       <div class="absolute bottom-8 right-8 text-xl">Contact</div>
     </div>
   );
