b9fe305fe28e701a2d6c102b651af5d8a644934a
kristopherkewish <148918881+kristopherkewish@users.noreply.github.com>
2024-05-22 14:17:31 +1000

    Debounce scroll handler

--- app/ScrollContainer.js
+++ app/ScrollContainer.js
@@ -1,33 +1,36 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import _ from "lodash";
+import Landing from "./_sections/_landing/Landing.js";
 
-export default function ScrollContainer({ children }) {
+export default function ScrollContainer() {
   const [windowHeight, setWindowHeight] = useState(window.innerHeight);
 
   const sections = ["landing", "skills", "projects", "experience", "contact"];
   let currentSectionIndex = 0;
 
   useEffect(() => {
-    const handleWheel = (event) => {
-      if (event.deltaY >= 100) {
+    const handleWheel = _.debounce((event) => {
+      // Use of Lodash's debounce ensures only one wheel event is triggered per scroll
+      if (true) {
         console.log("The current section is: ", sections[currentSectionIndex]);
 
         if (event.deltaY > 0) {
-          console.log('scrolled down!');
-          if(currentSectionIndex < sections.length-1) {
+          console.log("scrolled down!");
+          if (currentSectionIndex < sections.length - 1) {
             currentSectionIndex++;
           }
         } else {
-          console.log('scrolled up!');
-          if(currentSectionIndex > 0) {
+          console.log("scrolled up!");
+          if (currentSectionIndex > 0) {
             currentSectionIndex--;
           }
         }
 
         console.log("The next section is: ", sections[currentSectionIndex]);
       }
-    };
+    }, 200);
 
     const handleResize = () => setWindowHeight(window.innerHeight);
 
@@ -35,10 +38,14 @@ export default function ScrollContainer({ children }) {
     window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener("scroll", handleWheel);
+      window.removeEventListener("wheel", handleWheel);
       window.removeEventListener("resize", handleResize);
     };
   }, []);
 
-  return <div>{children}</div>;
+  return (
+    <>
+      <Landing />
+    </>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,18 +1,10 @@
 import ScrollContainer from "./ScrollContainer.js";
-import Landing from "./_sections/_landing/Landing.js";
+
 import Skills from "./_sections/_skills/page.js";
 import Projects from "./_sections/_projects/page.js";
 import Experience from "./_sections/_experience/page.js";
 import Contact from "./_sections/_contact/page.js";
 
 export default function Home() {
-  return (
-    <ScrollContainer>
-      <Landing />
-      <Skills />
-      <Projects />
-      <Experience />
-      <Contact />
-    </ScrollContainer>
-  );
+  return <ScrollContainer />
 }
