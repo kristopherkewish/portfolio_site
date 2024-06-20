343dc040633318eea9ccff2b9f516063ba6d1092
kristopherkewish <148918881+kristopherkewish@users.noreply.github.com>
2024-06-20 19:18:53 +1000

    Hide skills text on mobile screens

--- app/_sections/_skills/page.js
+++ app/_sections/_skills/page.js
@@ -6,39 +6,36 @@ export default function Skills() {
       id="skills"
       className="absolute flex h-dvh w-screen items-center justify-center overflow-hidden bg-blue-900 text-white"
     >
-      {
-        /*
+      <div className="hidden lg:block">
         <div className="absolute left-16 top-20 text-2xl font-bold">
-        LANGUAGES:
-        <ul className="text-l font-mono font-normal">
-          <li>Javascript</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>SQL</li>
-        </ul>
-        <br />
-        FRAMEWORKS:
-        <ul className="text-l font-mono font-normal">
-          <li>React</li>
-          <li>Tailwind</li>
-          <li>Bootstrap</li>
-          <li>Express</li>
-          <li>NodeJS</li>
-          <li>NextJS</li>
-        </ul>
-        <br />
-        TOOLS:
-        <ul className="text-l font-mono font-normal">
-          <li>Postgres</li>
-          <li>Postman</li>
-          <li>Github</li>
-        </ul>
+          LANGUAGES:
+          <ul className="text-l font-mono font-normal">
+            <li>Javascript</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>SQL</li>
+          </ul>
+          <br />
+          FRAMEWORKS:
+          <ul className="text-l font-mono font-normal">
+            <li>React</li>
+            <li>Tailwind</li>
+            <li>Bootstrap</li>
+            <li>Express</li>
+            <li>NodeJS</li>
+            <li>NextJS</li>
+          </ul>
+          <br />
+          TOOLS:
+          <ul className="text-l font-mono font-normal">
+            <li>Postgres</li>
+            <li>Postman</li>
+            <li>Github</li>
+          </ul>
+        </div>
       </div>
-        */
-      }
-      
       <OrbitContainer />
-      {/* <div className="absolute text-6xl font-extrabold">SKILLS</div> */}
+      <div className="absolute text-6xl font-extrabold">SKILLS</div>
     </div>
   );
 }
